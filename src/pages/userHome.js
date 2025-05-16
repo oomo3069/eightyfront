f3320d22c23b0b9cec35a02aee6e012e22fbd97b
oomo3069 <oomo1212312121@gmail.com>
2025-05-16 11:58:20 +0700

    hello

---
+++ src/pages/userHome.js
@@ -0,0 +1,12 @@
+import '../css/userHome.css'
+
+
+function userHome(){
+    return(
+        <div className='Homecontainer'>
+        <div>Hello</div>
+        </div>
+    );
+}
+
+export default  userHome;
